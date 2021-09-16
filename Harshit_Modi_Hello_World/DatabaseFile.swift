4365d2c2af0fc92e4937a5d0631bfa0afa72c9a3
Harshit Modi <harshitmodi1991@gmail.com>
2021-09-16 15:07:06 -0400

    Initial Commit

---
+++ Harshit_Modi_Hello_World/DatabaseFile.swift
@@ -0,0 +1,23 @@
+//
+//  DatabaseFile.swift
+//  Harshit_Modi_Hello_World
+//
+//  Created by Harshit on 14/09/21.
+//  Copyright © 2021 Harshit Modi. All rights reserved.
+//
+
+import Foundation
+import UIKit
+
+class DatabaseFile : UIViewController {
+    
+    func connect() {
+        
+    }
+    
+    func disconnect() {
+        
+    }
+    
+    
+}
---
+++ Harshit_Modi_Hello_World/ViewController.swift
@@ -0,0 +1,24 @@
+//
+//  ViewController.swift
+//  Harshit_Modi_Hello_World
+//
+//  Created by Harshit on 14/09/21.
+//  Copyright © 2021 Harshit Modi. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    @IBOutlet weak var btnNext: UIButton!
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+    @IBAction func btnNextAction(_ sender: Any) {
+        
+    }
+    
+}
+
